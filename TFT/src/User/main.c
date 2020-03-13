70066c17bf620a19376b38f527ec17154bdb30f4
Uz45 <leha-sotik@yandex.ru>
2020-03-13 21:01:47 +0500

    Update to 25.2 Uncomment MKSTFTV3M in variants.h for use if the image is mirrored

--- TFT/src/User/main.c
+++ TFT/src/User/main.c
@@ -6,22 +6,23 @@ MENU  infoMenu;  // Menu structure
 void Hardware_GenericInit(void)
 {
   NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-  Delay_init(F_CPUM);  
+  Delay_init(F_CPUM);
   OS_TimerInit(9999, F_CPUM-1);  // System clock timer, cycle 10ms
-  
-#ifdef DISABLE_DEBUG 
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO , ENABLE);
-  GPIO_PinRemapConfig(GPIO_Remap_SWJ_Disable, ENABLE); //disable JTAG & SWD
-#endif
- 
-#ifdef DISABLE_JTAG
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO , ENABLE);
-  GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);
-#endif
-#ifdef MKS_32_V1_4
-RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO,ENABLE);
-GPIO_PinRemapConfig(GPIO_Remap_USART2, ENABLE);
-#endif
+
+  #ifdef DISABLE_DEBUG
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO , ENABLE);
+    GPIO_PinRemapConfig(GPIO_Remap_SWJ_Disable, ENABLE); //disable JTAG & SWD
+  #endif
+
+  #ifdef DISABLE_JTAG
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO , ENABLE);
+    GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);
+  #endif
+
+  #if defined MKS_32_V1_4 || defined(MKS_32_V1_3)
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO,ENABLE);
+    GPIO_PinRemapConfig(GPIO_Remap_USART2, ENABLE);
+  #endif
 
   XPT2046_Init();
   W25Qxx_Init();
@@ -29,25 +30,31 @@ GPIO_PinRemapConfig(GPIO_Remap_USART2, ENABLE);
   readStoredPara();
   LCD_RefreshDirection();  //refresh display direction after reading settings
   scanUpdates();
-  #ifndef MKS_32_V1_4
-  //causes hang if we deinit spi1 
+#if defined (MKS_32_V1_4) || defined(MKS_32_V1_3)
+#else
+  //causes hang if we deinit spi1    
   SD_DeInit();
+#endif
+  #if LCD_ENCODER_SUPPORT
+    LCD_EncoderInit();
   #endif
-  
-#if LCD_ENCODER_SUPPORT
-  LCD_EncoderInit();
-#endif
 
-#ifdef PS_ON_PIN
-  PS_ON_Init();
-#endif
+  #ifdef PS_ON_PIN
+    PS_ON_Init();
+  #endif
 
-#ifdef FIL_RUNOUT_PIN
-  FIL_Runout_Init();
-#endif
+  #ifdef FIL_RUNOUT_PIN
+    FIL_Runout_Init();
+  #endif
+
+  #ifdef LED_color_PIN
+    knob_LED_Init();
+  #else
+    #define STARTUP_KNOB_LED_COLOR 1
+  #endif
 
   if(readStoredPara() == false) // Read settings parameter
-  {    
+  {
     TSC_Calibration();
     storePara();
   }
@@ -57,11 +64,10 @@ GPIO_PinRemapConfig(GPIO_Remap_USART2, ENABLE);
 
 int main(void)
 {
-
   SCB->VTOR = VECT_TAB_FLASH;
- 
+
   Hardware_GenericInit();
-  
+
   for(;;)
   {
     (*infoMenu.menu[infoMenu.cur])();
--- TFT/src/User/variants.h
+++ TFT/src/User/variants.h
@@ -11,7 +11,7 @@
 
 //MCU type (STM32F103VC - HD, STM32F105 - CL, STM32F207VC)
 //HSE crystal frequency
-#if defined(TFT35_V1_0) || defined(TFT35_V1_1) || defined(TFT35_V1_2)|| defined(TFT28_V1_0)  
+#if defined(TFT35_V1_0) || defined(TFT35_V1_1) || defined(TFT35_V1_2)|| defined(TFT28_V1_0)
   #define VECT_TAB_FLASH 0x08006000
   #define HSE_VALUE ((uint32_t)16000000) //16Mhz
   #define F_CPUM 72
@@ -35,12 +35,12 @@
   #define F_CPUM 120
   #define STM32F2XX
   #include "stm32f2xx.h"
-#elif defined(MKS_32_V1_4)
+#elif defined(MKS_32_V1_4) || defined(MKS_32_V1_3)
 
   #define HSE_VALUE ((uint32_t)25000000) //25Mhz XTAL
   #define F_CPUM 48
   #define STM32F10X_CL
-  #include "stm32f10x.h"  
+  #include "stm32f10x.h"
 #endif
 
 //LCD interface
@@ -48,7 +48,7 @@
   #define RM68042
   #define STM32_HAS_FSMC
   #define LCD_DATA_16BIT 0
-#elif defined(TFT35_V1_2) || defined(TFT35_V2_0) || defined(TFT35_V3_0) 
+#elif defined(TFT35_V1_2) || defined(TFT35_V2_0) || defined(TFT35_V3_0)
   #define ILI9488
   #define STM32_HAS_FSMC
   #define LCD_DATA_16BIT 1
@@ -62,6 +62,10 @@
 #elif defined(MKS_32_V1_4)
   #define HX8558
   #define LCD_DATA_16BIT 1
+#elif defined(MKS_32_V1_3)
+  #define MKSTFTV3
+  //#define MKSTFTV3M //If the image is mirrored
+  #define LCD_DATA_16BIT 1
 #endif
 
 //LCD Backlight pin (PWM can adjust brightness)
@@ -69,8 +73,8 @@
   #define LCD_LED_PIN   PA8
 #elif defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define LCD_LED_PIN   PD12
-#elif defined(MKS_32_V1_4)
- #define LCD_LED_PIN    PD14  
+#elif defined(MKS_32_V1_4) || defined(MKS_32_V1_3)
+ #define LCD_LED_PIN    PD14
 #endif
 
 #if defined(TFT35_V1_0) || defined(TFT35_V1_1) || defined(TFT35_V1_2) || defined(TFT28_V1_0) || defined(TFT24_V1_1)
@@ -84,10 +88,10 @@
   #define SERIAL_PORT_2 _USART1
   #define SERIAL_PORT_3 _USART3
   #define SERIAL_PORT_4 _UART4
-#elif defined(MKS_32_V1_4)
+#elif defined(MKS_32_V1_4) || defined(MKS_32_V1_3)
   #define SERIAL_PORT   _USART2
   #define SERIAL_PORT_2 _USART1
-  #define SERIAL_PORT_3 _USART3  
+  #define SERIAL_PORT_3 _USART3
 #endif
 
 //XPT2046 Software SPI Pins (touch screen ic)
@@ -110,18 +114,18 @@
   #define XPT2046_MISO  PB4
   #define XPT2046_MOSI  PB5
   #define XPT2046_TPEN  PC1
-#elif defined(TFT35_V3_0) || defined(TFT28_V3_0) 
+#elif defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define XPT2046_CS    PE6
   #define XPT2046_SCK   PE5
   #define XPT2046_MISO  PE4
   #define XPT2046_MOSI  PE3
   #define XPT2046_TPEN  PC13
-#elif defined(MKS_32_V1_4)
+#elif defined(MKS_32_V1_4) || defined(MKS_32_V1_3)
   #define XPT2046_CS    PC9
   #define XPT2046_SCK   PC10
   #define XPT2046_MISO  PC11
   #define XPT2046_MOSI  PC12
-  #define XPT2046_TPEN  PC5  
+  #define XPT2046_TPEN  PC5
 #else
   #error "xpt2046 need to Implement here by yourself."
 #endif
@@ -141,12 +145,12 @@
   #define SPI1_CS_PIN   PA4
 #elif defined(TFT35_V2_0)
   #define SD_SDIO_SUPPORT
-#elif defined(MKS_32_V1_4)
+#elif defined(MKS_32_V1_4) || defined(MKS_32_V1_3)
   #define SD_SPI_SUPPORT
   #define SD_LOW_SPEED  7
   #define SD_HIGH_SPEED 1
   #define SD_SPI        _SPI1
-  #define SPI1_CS_PIN   PD11  
+  #define SPI1_CS_PIN   PD11
 #endif
 
 //SD Card CD detect pin
@@ -158,8 +162,8 @@
   #define SD_CD_PIN     PC6
 #elif defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define SD_CD_PIN     PC4
-#elif defined(MKS_32_V1_4)
-  #define SD_CD_PIN     PB15  
+#elif defined(MKS_32_V1_4) || defined(MKS_32_V1_3)
+  #define SD_CD_PIN     PB15
 #endif
 
 //W25Qxx SPI pins
@@ -175,18 +179,18 @@
   #define W25Qxx_SPEED  1
   #define W25Qxx_SPI    _SPI3
   #define SPI3_CS_PIN   PB6
-#elif defined(MKS_32_V1_4)
+#elif defined(MKS_32_V1_4) || defined(MKS_32_V1_3)
   #define W25Qxx_SPEED  1
   #define W25Qxx_SPI    _SPI1
-  #define W25Qxx_CS     PB9  
+  #define W25Qxx_CS     PB9
 #endif
 
 //ST7920 Simulator SPI pins
 #if defined(TFT24_V1_1) || defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define ST7920_SPI    _SPI2
-#elif defined(MKS_32_V1_4) 
+#elif defined(MKS_32_V1_4) || defined(MKS_32_V1_3)
   //#define ST7920_SPI    _SPI1
-  //#define SPISIM_CS_PIN    PB4  
+  //#define SPISIM_CS_PIN    PB4
 #endif
 
 //buzzer support
@@ -196,25 +200,25 @@
   #define BUZZER_PIN    PB2
 #elif defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define BUZZER_PIN    PD13
-#elif defined(MKS_32_V1_4)
-  #define BUZZER_PIN    PA2  
+#elif defined(MKS_32_V1_4) || defined(MKS_32_V1_3)
+  #define BUZZER_PIN    PA2
 #endif
 
 //LCD Encoder support
 #if defined(TFT24_V1_1)
-  //PB0:ENC-A PB1:ENC-B PB2:BTN 
+  //PB0:ENC-A PB1:ENC-B PB2:BTN
   #define LCD_ENCA_PIN  PB0
   #define LCD_ENCB_PIN  PB1
   #define LCD_BTN_PIN   PB2
 #elif defined(TFT35_V3_0) || defined(TFT28_V3_0)
-  //PA8:ENC-A PC9:ENC-B PC8:BTN 
+  //PA8:ENC-A PC9:ENC-B PC8:BTN
   #define LCD_ENCA_PIN  PA8
   #define LCD_ENCB_PIN  PC9
   #define LCD_BTN_PIN   PC8
-#elif defined(MKS_32_V1_4)
+#elif defined(MKS_32_V1_4) || defined(MKS_32_V1_3)
   #define LCD_ENCA_PIN  PB0
   #define LCD_ENCB_PIN  PB1
-  #define LCD_BTN_PIN   PB5  
+  #define LCD_BTN_PIN   PB5
 #endif
 #define LCD_ENCODER_SUPPORT (defined(LCD_ENCA_PIN) && defined(LCD_ENCB_PIN) && defined(LCD_BTN_PIN))
 
@@ -222,9 +226,9 @@
 #if defined(TFT24_V1_1) || defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define U_DISK_SUPPROT
   #define USE_USB_OTG_FS
-#elif defined(MKS_32_V1_4)
+#elif defined(MKS_32_V1_4) || defined(MKS_32_V1_3)
   #define U_DISK_SUPPROT
-  #define USE_USB_OTG_FS  
+  #define USE_USB_OTG_FS
 #endif
 
 //extend function(PS_ON, filament_detect)
@@ -234,11 +238,11 @@
 #elif defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define PS_ON_PIN      PA12
   #define FIL_RUNOUT_PIN PA15
-#elif defined(MKS_32_V1_4)
+#elif defined(MKS_32_V1_4) || defined(MKS_32_V1_3)
   //#define PS_ON_PIN      PB0
-  //#define FIL_RUNOUT_PIN PB1  
+  //#define FIL_RUNOUT_PIN PB1
 #endif
-    
+
 //Debug disable, free pins for other function
 #if defined(TFT35_V1_0) || defined(TFT35_V1_1) || defined(TFT35_V1_2) || defined(TFT28_V1_0) || defined(TFT35_V2_0)
   #define DISABLE_JTAG    //free JTAG(PB3/PB4) for SPI3
@@ -252,15 +256,15 @@
 #if defined(TFT35_V1_0) || defined(TFT35_V1_1) || defined(TFT35_V1_2) || defined(TFT35_V2_0) || defined(TFT35_V3_0)
   #define LCD_WIDTH	  480
   #define LCD_HEIGHT	320
-  
+
   #define BYTE_HEIGHT 24
   #define BYTE_WIDTH  (BYTE_HEIGHT/2)
-  
+
   #define ICON_WIDTH   95
   #define ICON_HEIGHT  95
   #define TITLE_END_Y  40
   #define ICON_START_Y (TITLE_END_Y+10)
-  
+
   #define LIST_ICON_WIDTH   85
   #define LIST_ICON_HEIGHT  70
 
@@ -271,50 +275,50 @@
   #define SMALLICON_HEIGHT   24
 
   #define selecticonw 95
-  
+
 #elif defined(TFT28_V1_0) || defined(TFT24_V1_1) || defined(TFT28_V3_0)
   #define LCD_WIDTH	  320
   #define LCD_HEIGHT	240
-  
+
   #define BYTE_HEIGHT 16
   #define BYTE_WIDTH  (BYTE_HEIGHT/2)
-  
+
   #define ICON_WIDTH  70
   #define ICON_HEIGHT 70
   #define TITLE_END_Y  30
   #define ICON_START_Y (TITLE_END_Y+10)
-  
+
   #define LIST_ICON_WIDTH   55
   #define LIST_ICON_HEIGHT  50
-  
+
   #define INFOBOX_WIDTH     150
   #define INFOBOX_HEIGHT    70
 
   #define SMALLICON_WIDTH   16
   #define SMALLICON_HEIGHT   16
-  
+
   #define selecticonw 70
-#elif defined(MKS_32_V1_4)
+#elif defined(MKS_32_V1_4) || defined(MKS_32_V1_3)
   #define LCD_WIDTH	  320
   #define LCD_HEIGHT	240
-  
+
   #define BYTE_HEIGHT 16
   #define BYTE_WIDTH  (BYTE_HEIGHT/2)
-  
+
   #define ICON_WIDTH  70
   #define ICON_HEIGHT 70
   #define TITLE_END_Y  30
   #define ICON_START_Y (TITLE_END_Y+10)
-  
+
   #define LIST_ICON_WIDTH   55
   #define LIST_ICON_HEIGHT  50
-  
+
   #define INFOBOX_WIDTH     150
   #define INFOBOX_HEIGHT    70
 
   #define SMALLICON_WIDTH   16
   #define SMALLICON_HEIGHT   16
-  
+
   #define selecticonw 70
 #endif
 
@@ -330,14 +334,14 @@
   #define ROOT_DIR "TFT28"
 #elif defined(TFT24_V1_1)
   #define ROOT_DIR "TFT24"
-#elif defined(MKS_32_V1_4)
-  #define ROOT_DIR "MKS"  
+#elif defined(MKS_32_V1_4) || defined(MKS_32_V1_3)
+  #define ROOT_DIR "MKS"
 #endif
 
 //hardware version config
 #ifndef HARDWARE_VERSION
   #if defined(TFT35_V1_0)
-    #define HARDWARE_VERSION "TFT35_V1.0"   
+    #define HARDWARE_VERSION "TFT35_V1.0"
   #elif defined(TFT35_V1_1)
     #define HARDWARE_VERSION "TFT35_V1.1"
   #elif defined(TFT35_V1_2)
@@ -354,7 +358,9 @@
     #define HARDWARE_VERSION "TFT28_V3.0"
   #elif defined(MKS_32_V1_4)
     #define HARDWARE_VERSION "TFT32_V4.0"
-  #endif  
+  #elif defined(MKS_32_V1_3)
+    #define HARDWARE_VERSION "TFT32_V3.0"
+  #endif
 #endif
 
 #endif
