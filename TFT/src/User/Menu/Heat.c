040cb4e6aefdb317f97e3af1faa41927f019e7a9
Uz45 <leha-sotik@yandex.ru>
2020-05-25 19:55:56 +0500

    Fix setting current nozzle logic in Heat.c, Fix text display in Popup window

--- TFT/src/User/Menu/Heat.c
+++ TFT/src/User/Menu/Heat.c
@@ -134,7 +134,7 @@ TOOL heatGetCurrentTool(void)
 /* Set current nozzle */
 void heatSetCurrentToolNozzle(TOOL tool)
 {
-  if(tool >= (infoSettings.tool_count + 1) && tool < NOZZLE0) return;
+  if(tool >= (infoSettings.tool_count + 1) || tool < NOZZLE0) return;
   heater.nozzle = tool;
   heater.tool = tool;
 }
